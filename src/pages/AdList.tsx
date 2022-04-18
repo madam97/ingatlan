1303358ac07bf654388764267ec39bf571380db6
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-18 20:16:00 +0200

    added ad data fetching, Heading component on AdList page

--- src/pages/AdList.tsx
+++ src/pages/AdList.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Heading from '../components/Heading';
+import IAd from '../interfaces/IAd';
 
 export default function AdList() {
+
+  const [ads, setAds] = useState<IAd[]>([]);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const res = await axios.get(process.env.REACT_APP_API_BASE_URL + '/ads');
+
+        if (res.data) {
+          setAds(res.data);
+        }
+      } catch(err) {
+
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  // ------------------------------------------
+
   return (
-    <main>AdList</main>
+    <main className="pl-main-menu">
+      <Container fluid>
+        <Heading title="Lista" count={ads.length} />
+      </Container>
+    </main>
   )
 }
