f026cb646921e9d87d925466ce2c19d89b7203b6
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-18 17:39:31 +0200

    added router usage and route of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Footer from './pages/parts/Footer';
 import Header from './pages/parts/Header';
+import AdList from './pages/AdList';
+import AdDetailed from './pages/AdDetailed';
+import FavoriteList from './pages/FavoriteList';
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<AdList />} />
+        <Route path="/ads/:id" element={<AdDetailed />} />
+        <Route path="/favorites" element={<FavoriteList />} />
+      </Routes>
       <Footer />
     </>
   );
