4c2f742ca092fc398d92cbd5cda7be4fb822a201
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-18 18:50:21 +0200

    added main menu in Header

--- src/pages/parts/Header.tsx
+++ src/pages/parts/Header.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { ListUl, StarFill } from 'react-bootstrap-icons';
 
 export default function Header() {
   return (
     <header>
-      <Navbar className="shadow rounded-bottom" bg="light" expand="lg" fixed="top">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        </Container>
+      <Navbar className="shadow rounded-bottom" bg="light" expand="md" fixed="top">
+        <Navbar.Toggle aria-controls="main-menu" />
+
+        <Navbar.Collapse id="main-menu" className="fixed-top">
+          <Nav defaultActiveKey="/" className="flex-column p-3 w-100 rounded bg-light shadow">
+            <LinkContainer to="/">
+              <Nav.Link>
+                <ListUl /> Lista
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/favorites">
+              <Nav.Link>
+                <StarFill /> Kedvencek
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </header>
   )
