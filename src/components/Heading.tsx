3a3f70314dde4f3cbf67586bfce5b37b3f00ef93
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-19 13:37:21 +0200

    changed bootstrap select input's style changed Heading component to be able to display order select input

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import TOrder from '../types/TOrder';
 
 type HeadingProps = {
   title: string,
-  count?: number
+  count?: number,
+  activeOrder?: string,
+  orders?: TOrder[],
+  setActiveOrder?: (value: string) => void
 }
 
-export default function Heading({ title, count }: HeadingProps) {
+export default function Heading({ title, count, activeOrder = '', orders = [], setActiveOrder = () => {} }: HeadingProps) {
+
+  const handleOrderChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    e.preventDefault(); 
+    setActiveOrder(e.target.value);
+  }
+
+  // ------------------------------------------
+
   return (
     <div className="component-heading rounded bg-light shadow">
       <h1>{title}</h1>
+
       {count && <span>{count} db</span>}
+
+      {orders && 
+        <Form.Select className="shadow-lg" value={activeOrder} onChange={handleOrderChange}>
+          <option value="">Rendezés</option>
+
+          {orders.map(order => <option key={order.title} value={order.id}>{order.title}</option>)}
+        </Form.Select>
+      }
     </div>
   )
 }
