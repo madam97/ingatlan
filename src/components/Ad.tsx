1609509aca34402f2c363d7bbc20b3df47a487b8
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-19 11:50:15 +0200

    changed Ad component to display upload date if needed both desktop and mobile view

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Calendar } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
 import IAd from '../interfaces/IAd';
 import FavoriteButton from './FavoriteButton';
 
 type AdProps = {
-  ad: IAd
+  ad: IAd,
+  showUploadDate?: boolean
 }
 
-export default function Ad({ ad }: AdProps) {
+export default function Ad({ ad, showUploadDate = false }: AdProps) {
   return (
     <Card className="ad-component mt-3 border-0 shadow-lg">
       <Card.Img src={ad.image} />
       <Card.Body>
         <Card.Title>{ad.address}</Card.Title>
-        <Card.Text>
-          <p className="subheading-font">{ad.price.toLocaleString('hu-HU')} {ad.currency}</p>
-
-          <FavoriteButton adId={ad.adId} status={ad.status} />
-        </Card.Text>
+        <Card.Text className="ad-component-price">{ad.price.toLocaleString('hu-HU')} {ad.currency}</Card.Text>
+        {showUploadDate && 
+          <Card.Text className="ad-component-date">
+            <Calendar /> {ad.uploadDate}
+          </Card.Text>
+        }
+        <FavoriteButton adId={ad.adId} status={ad.status} />
       </Card.Body>
 
       <Link to={`/ads/${ad.adId}`}></Link>
