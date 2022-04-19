973aa91a6c622fc56e9efd32217c86c5a9ff2e04
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-19 13:40:07 +0200

    added sorting of ads to FavoriteList page

--- src/pages/FavoriteList.tsx
+++ src/pages/FavoriteList.tsx
@@ -4,11 +4,14 @@ import { fetchAds } from '../models/AdModel';
 import Ad from '../components/Ad';
 import Heading from '../components/Heading';
 import IAd from '../interfaces/IAd';
+import TOrder from '../types/TOrder';
 
 export default function FavoriteList() {
 
   const [ads, setAds] = useState<IAd[]>([]);
+  const [activeOrder, setActiveOrder] = useState<string>('');
 
+  /** Fetches ads */
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       const res = await fetchAds({ status: 'checked' });
@@ -21,12 +24,45 @@ export default function FavoriteList() {
     fetchData();
   }, []);
 
+  /** Sorts ads */
+  useEffect(() => {
+    const newAds = ads.slice();
+
+    if (!activeOrder) {
+      newAds.sort((ad1, ad2) => ad1.adId < ad2.adId ? -1 : 1);
+    } else {
+      newAds.sort((ad1, ad2) => {
+        switch (activeOrder) {
+          case 'addressAsc':
+            return ad1.address.localeCompare(ad2.address, 'hu-HU');
+          case 'addressDesc':
+            return -1 * ad1.address.localeCompare(ad2.address, 'hu-HU');
+          case 'uploadDateAsc':
+            return ad1.uploadDate === ad2.uploadDate ? 0 : (ad1.uploadDate < ad2.uploadDate ? -1 : 1);
+          case 'uploadDateDesc':
+            return ad1.uploadDate === ad2.uploadDate ? 0 : (ad1.uploadDate < ad2.uploadDate ? 1 : -1);
+          default:
+            return 0;
+        }
+      });
+    }
+
+    setAds(newAds);
+  }, [activeOrder]);
+
   // ------------------------------------------
+
+  const orders: TOrder[] = [
+    { id: 'addressAsc', title: 'Cím szerint növekvő' },
+    { id: 'addressDesc', title: 'Cím szerint csökkenő' },
+    { id: 'uploadDateAsc', title: 'Feltöltés szerint növekvő' },
+    { id: 'uploadDateDesc', title: 'Feltöltés szerint csökkenő' },
+  ]
 
   return (
     <main className="pl-main-menu">
       <Container fluid>
-        <Heading title="Kedvencek" count={ads.length} />
+        <Heading title="Kedvencek" activeOrder={activeOrder} orders={orders} setActiveOrder={setActiveOrder} />
 
         {ads.map(ad => <Ad key={ad.adId} ad={ad} showUploadDate={true} />)}
       </Container>
