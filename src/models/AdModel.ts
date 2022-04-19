4e6b462a893ad18aa2bc0501b65b16deb9c09543
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-19 09:08:17 +0200

    added AdModel

---
+++ src/models/AdModel.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import IAd from '../interfaces/IAd';
+
+/** 
+ * The API endpoint of the ads
+ * @const {string}
+ */
+const AD_URL = process.env.REACT_APP_API_BASE_URL + '/ads';
+
+/**
+ * Returns ads' data
+ * @returns {Promise<IAd[] | string>}
+ */
+const fetchAds = async (): Promise<IAd[] | string> => {
+  try {
+    const res = await axios.get(AD_URL);
+
+    return res.data ?? 'no data returned';
+  } catch(err) {
+    return err instanceof Error ? err.message : 'unknown error';
+  }
+}
+
+/**
+ * Update the given ad's data
+ * @param id 
+ * @param data 
+ * @returns {Promise<IAd | string>}
+ */
+const patchAd = async (id: number, data: object): Promise<IAd | string> => {
+  try {
+    const res = await axios.patch(AD_URL + '/' + id, data);
+
+    return res.data ?? 'no data returned';
+  } catch(err) {
+    return err instanceof Error ? err.message : 'unknown error';
+  }
+} 
+
+export { AD_URL, fetchAds, patchAd };
