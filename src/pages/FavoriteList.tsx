1e46b83f3fe70c7f580b98b1aad4346343219374
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-19 11:50:34 +0200

    added content to FavoriteList page

--- src/pages/FavoriteList.tsx
+++ src/pages/FavoriteList.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { fetchAds } from '../models/AdModel';
+import Ad from '../components/Ad';
+import Heading from '../components/Heading';
+import IAd from '../interfaces/IAd';
 
 export default function FavoriteList() {
+
+  const [ads, setAds] = useState<IAd[]>([]);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const res = await fetchAds({ status: 'checked' });
+
+      if (typeof res !== 'string') {
+        setAds(res);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  // ------------------------------------------
+
   return (
-    <main>FavoriteList</main>
+    <main className="pl-main-menu">
+      <Container fluid>
+        <Heading title="Kedvencek" count={ads.length} />
+
+        {ads.map(ad => <Ad key={ad.adId} ad={ad} showUploadDate={true} />)}
+      </Container>
+    </main>
   )
 }
